a20d30db3e01e48ee28ba4833380bce56cefbb92
RiaLia <rialia@live.se>
2018-03-15 11:47:36 +0100

    Ground design, custom cell.

---
+++ iWeather/CustomTableCell.swift
@@ -0,0 +1,33 @@
+//
+//  CustomTableCellTableViewCell.swift
+//  iWeather
+//
+//  Created by Ria Buhlin on 2018-03-14.
+//  Copyright © 2018 Ria Buhlin. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableCell: UITableViewCell {
+
+    @IBOutlet weak var cityText: UILabel!
+    @IBOutlet weak var descText: UILabel!
+    @IBOutlet weak var windText: UILabel!
+
+    @IBOutlet weak var tempField: UILabel!
+    @IBOutlet weak var dateText: UILabel!
+    @IBOutlet weak var iconImage: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iWeather/WeatherTableViewController.swift
@@ -0,0 +1,96 @@
+//
+//  WeatherTableViewController.swift
+//  iWeather
+//
+//  Created by Ria Buhlin on 2018-03-13.
+//  Copyright © 2018 Ria Buhlin. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherTableViewController: UITableViewController, UISearchBarDelegate {
+    
+    let cities = ["Göteborg", "Stockholm", "Malmö", "Sundsvall", "Karlstad"]
+    let weather = ["Sunny", "Rain" ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! CustomTableCell
+
+        // Här skulle jag vilja hämta in CustumTableCell och sen ändra innehållet
+        //let custom = CustomTableCell()
+        let cityName = cities[indexPath.row]
+        cell.cityText.text = cityName
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
